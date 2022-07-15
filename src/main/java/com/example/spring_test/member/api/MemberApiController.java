ddd81815112bf36515613c9674c85a7ade3dcc9e
SeoHyeonMyeong <gusaud2199@naver.com>
2022-07-15 22:04:45 +0900

    Add: makes some default Class for App

---
+++ src/main/java/com/example/spring_test/member/api/MemberApiController.java
@@ -0,0 +1,13 @@
+package com.example.spring_test.member.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
+public class MemberApiController {
+
+}
